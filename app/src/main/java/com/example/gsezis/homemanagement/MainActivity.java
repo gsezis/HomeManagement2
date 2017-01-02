b87c2a3e86fe4ff98944c095b3df27ba5d634383
Gueven Sezis <inf2865@hs-worms.de>
2017-01-02 23:06:05 +0100

    added the login view

--- app/src/main/java/com/example/gsezis/homemanagement/MainActivity.java
+++ app/src/main/java/com/example/gsezis/homemanagement/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.gsezis.homemanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editTextMail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextMail = (EditText) findViewById(R.id.Login_email);
+        editTextPassword = (EditText) findViewById(R.id.Login_password);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRegister(view);
+
+
+            }
+        });
     }
-}
+
+        public void setRegister(View view)
+    {
+
+        String email =editTextMail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter password",Toast.LENGTH_SHORT).show();
+
+            return;
+
+        }
+        progressDialog.setMessage("Registered User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Registered succusfully", Toast.LENGTH_SHORT).show();
+                        }else{
+
+                            Toast.makeText(MainActivity.this,"Could not registered, please try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                });
+    }
+    }
+
